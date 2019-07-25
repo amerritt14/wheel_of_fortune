9b485f557b52e6997f5634ca15a8bc241be58449
Andrew Merritt <amerritt14@liberty.edu>
2019-07-25 09:21:17 -0400

    Introduce turns

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -43,6 +43,7 @@ class RoundsController < ApplicationController
   def update
     respond_to do |format|
       if @round.make_guess(params["round"]["guessed_letter"])
+        update_current_player
         format.html { redirect_to game_round_path(@game, @round) }
         format.json { render :show, status: :created, location: @round }
       else
@@ -75,5 +76,9 @@ class RoundsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def round_params
       params.require(:round).permit(:game_id)
+    end
+
+    def update_current_player
+      @round.update(current_player_id(@round.find_next_player))
     end
 end
